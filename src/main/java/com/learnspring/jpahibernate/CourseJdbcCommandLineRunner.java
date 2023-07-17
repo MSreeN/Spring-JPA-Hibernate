05e103bfc423960cc698d7b8f52625960b45e7bb
MSreeN <khyathimnsree369@gmail.com>
2023-07-17 22:10:42 +0530

    implemented jpa methods

--- src/main/java/com/learnspring/jpahibernate/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/learnspring/jpahibernate/CourseJdbcCommandLineRunner.java
@@ -2,26 +2,31 @@ package com.learnspring.jpahibernate;
 
 import com.learnspring.jpahibernate.course.CourseJdbcRepository;
 import com.learnspring.jpahibernate.course.CourseJpaRepository;
+import com.learnspring.jpahibernate.course.SpringDataJpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Autowired
     CourseJdbcRepository courseJdbcRepository;
 
+//    @Autowired
+//    CourseJpaRepository courseJpaRepository;
     @Autowired
-    CourseJpaRepository courseJpaRepository;
+    CourseSpringDataJpaRepository repository;
     @Override
     public void run(String... args) throws Exception {
-        courseJpaRepository.insert(new Course(1, "Learn Aws now jpa", "Sree"));
-        courseJpaRepository.insert(new Course(2, "Learn Kubernetes now jpa", "Sree"));
-        courseJpaRepository.insert(new Course(3, "Learn Docker now jpa", "Sree"));
-        courseJpaRepository.deleteById(3L);
+        repository.save(new Course(1, "Learn Aws now jpa", "Sree"));
+        repository.save(new Course(2, "Learn Kubernetes now jpa", "Sree"));
+        repository.save(new Course(3, "Learn Docker now jpa", "Sree"));
+        repository.deleteById(3L);
 
-        Course course = courseJpaRepository.findById(2L);
+        Optional<Course> course = repository.findById(2L);
         System.out.println("-----------------");
         System.out.println(course);
     }
