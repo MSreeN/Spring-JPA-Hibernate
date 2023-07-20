55d315d15f28330a8a9f69e113d3540a69a97fd9
MSreeN <khyathimnsree369@gmail.com>
2023-07-20 23:52:26 +0530

    implemented sequence generator

--- src/main/java/com/learnspring/jpahibernate/Course.java
+++ src/main/java/com/learnspring/jpahibernate/Course.java
@@ -3,11 +3,17 @@ package com.learnspring.jpahibernate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import org.springframework.stereotype.Component;
 
 @Entity
 public class Course {
     @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            initialValue = 1
+    )
     private long id;
     @Column(
             name = "name"
@@ -16,6 +22,8 @@ public class Course {
     private String author;
 
     public Course(long id, String name, String Author) {
+
+
         this.id = id;
         this.name = name;
         this.author = Author;
